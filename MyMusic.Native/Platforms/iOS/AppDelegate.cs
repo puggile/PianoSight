d5a2cb64fd0f5a53d07668b190f15faf8aeb2921
Lorenzo Filippini <lorenzo.filippini@omniacom.it>
2026-02-17 10:21:39 +0100

    clean-up

--- MyMusic.Native/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MyMusic.Native;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
